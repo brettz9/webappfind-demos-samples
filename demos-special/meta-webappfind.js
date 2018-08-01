9267d6bca8f73e57e7357baf1eced0ee7f6f129e
Brett Zamir <brettz9@yahoo.com>
2018-07-31 17:36:58 -0700

    INCOMPLETE: SVG having problems; also retest HTML - Move to dynamic building of meta elements for better readability and expandability (with reusable library)

---
+++ demos-special/meta-webappfind.js
@@ -0,0 +1,37 @@
+/**
+* @typedef {object} PlainObject
+*/
+/**
+* @typedef {null|boolean|string|module:MetaWebAppFind.JSON[]|PlainObject.<string, module:MetaWebAppFind.JSON>} module:MetaWebAppFind.JSON
+*/
+/**
+* @typedef {PlainObject.<string, string|string[]|module:MetaWebAppFind.JSON>} module:MetaWebAppFind.Params
+*/
+
+/**
+ * @param {module:MetaWebAppFind.Params} params
+ * @returns {string}
+ */
+export function serializeParams (params) {
+  return Object.entries(params).reduce((s, [key, value]) => {
+    if (Array.isArray(value)) {
+      value = value.join(' ');
+    } else if (value && typeof value === 'object') {
+      value = JSON.stringify(value);
+    }
+    return `${s}&${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+  }, '').slice(1);
+}
+
+/**
+ * @param {module:MetaWebAppFind.Params[]} metaInfos
+ * @returns {undefined}
+ */
+export function addMetas (metaInfos) {
+    metaInfos.forEach((params) => {
+      const meta = document.createElement('meta');
+      meta.name = 'webappfind';
+      meta.content = serializeParams(params);
+      document.head.appendChild(meta);
+    });
+}
